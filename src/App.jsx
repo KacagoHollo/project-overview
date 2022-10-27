0d16b914409f509ce5f67780a2685f998201f368
IstvanPongracz <pongracz88@gmail.com>
2022-10-27 02:10:09 +0200

    navbar, basic design to nav, basic mui

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Wizard />}/>
+          <Route path='/new' element={<Wizard />}/>
         </Routes>
   
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from "@mui/material/Button";
 
-function Navbar() {
+const Navbar = () => {
+    const navigate = useNavigate();
+    const nav = (path) => {
+        console.log('rerouting');
+        navigate(path);
+    }
   return (
-    <div>Navbar</div>
+    <nav className='navbar' style={{backgroundColor:"#3d6487", height: "40px", display:"flex", alignItems: "center",justifyContent:"space-between"}}>
+        <div className='left'> 
+        <Button 
+            onClick={() => nav('/')}
+            variant="contained"
+            color="info"
+            size="small"
+            >
+              Home</Button>
+        <Button 
+            onClick={() => nav('/new')}
+            variant="contained"
+            color="info"
+            size="small"
+            >
+              Add New Project</Button>
+        </div>
+    </nav>
   )
 }
 
